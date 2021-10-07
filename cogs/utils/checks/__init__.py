891378712e2c359f1c32a1dc81f09c0014ab6348
schlopp <god.tcs@gmail.com>
2021-10-07 23:41:54 +0200

    Add check for slash commands

---
+++ cogs/utils/checks/__init__.py
@@ -0,0 +1,1 @@
+from .is_slash_command import *
---
+++ cogs/utils/checks/is_slash_command.py
@@ -0,0 +1,24 @@
+import typing
+
+from discord.ext import commands
+
+
+class IsNotSlashCommand(commands.CheckFailure):
+    """
+    The generic error for the bot failing the :func:`utils.checks.is_slash_command` check.
+    """
+
+
+def is_slash_command():
+    """
+    The check for whether or not the bot has processed all of its startup methods (as defined by `ctx` being an instance of :class:`discord.ext.commands.SlashContext`).
+
+    Raises:
+        `IsNotSlashCommand`: If the command isn't being used in a slash command.
+    """
+
+    async def predicate(ctx: typing.Union[commands.Context, commands.SlashContext]):
+        if isinstance(ctx.bot, commands.SlashContext):
+            return True
+        raise IsNotSlashCommand()
+    return commands.check(predicate)
