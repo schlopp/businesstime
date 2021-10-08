616638345b3cdc96ca7aaa92b6086dec5b0b0246
schlopp <god.tcs@gmail.com>
2021-10-08 18:37:12 +0200

    Update fonts

--- cogs/utils/consts.py
+++ cogs/utils/consts.py
@@ -7,9 +7,13 @@ DEFAULT_CARD_WIDTH: int = 600
 DEFAULT_CARD_HEIGHT: int = 400
 DEFAULT_CARD_SIZE: typing.Tuple[int, int] = (DEFAULT_CARD_WIDTH, DEFAULT_CARD_HEIGHT)
 
-DEFAULT_FONT_SIZE = 20
 
-ROBOTO_LIGHT: ImageFont.ImageFont = ImageFont.truetype(
-    "assets/fonts/Roboto-Light.ttf", size=DEFAULT_FONT_SIZE
-)
-DEFAULT_FONT: ImageFont.ImageFont = ROBOTO_LIGHT
+MIN_FONT_SIZE: int = 5
+MAX_FONT_SIZE: int = 50
+DEFAULT_FONT_SIZE: dict = 26
+
+ROBOTO_LIGHT = {}
+for i in range(MIN_FONT_SIZE, MAX_FONT_SIZE + 1):
+    ROBOTO_LIGHT[i] = ImageFont.truetype("assets/fonts/Roboto-Light.ttf", size=i)
+
+DEFAULT_FONT: dict = ROBOTO_LIGHT
